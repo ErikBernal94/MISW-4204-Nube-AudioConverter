98ce56e0cfc03c55a0c69dfcdec78ca0adc9871f
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2022-10-29 00:42:16 -0500

    Changes for clod db connection

--- AudioConverterAPI/app.py
+++ AudioConverterAPI/app.py
@@ -9,10 +9,25 @@ from views.file import FilesView
 from views.task import TasksIdView, TasksView
 
 app = Flask(__name__)
+
+# conexion a cloud
+
+PASSWORD ="admin"
+PUBLIC_IP_ADDRESS ="34.66.98.169"
+DBNAME ="audioconverter"
+ 
+# configuration
+# app.config["SECRET_KEY"] = "yoursecretkey"
+# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+ 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/AudioConverter'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://{}:{}@{}/{}'.format('postgres', PASSWORD, PUBLIC_IP_ADDRESS, DBNAME)
+# postgresql+pg8000://<db_user>:<db_pass>@/<db_name>
+        #                         ?unix_sock=<INSTANCE_UNIX_SOCKET>/.s.PGSQL.5432
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://{}:{}@/{}?unix_sock={}/.s.PGSQL.5432'.format('postgres', PASSWORD, DBNAME, 'misw4204-desarrollo-nube:us-central1:audioconverter')
 app_context = app.app_context()
 app_context.push()
 
