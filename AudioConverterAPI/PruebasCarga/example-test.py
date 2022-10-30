cad46bfdfc6e4e08fbb0bf08bc8937b933b12dfd
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2022-10-30 11:15:58 -0500

    Stress test

--- AudioConverterAPI/PruebasCarga/example-test.py
+++ AudioConverterAPI/PruebasCarga/example-test.py
@@ -2,27 +2,28 @@ from locust import HttpUser, task, between
 import requests
 import random
 
+token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2NzE0NjM0OSwianRpIjoiYjVmNzgwM2EtODRkYy00ZTdkLThlNzctNGJiZjE0OTcyYjgzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjY3MTQ2MzQ5LCJleHAiOjE2NjcxNDcyNDl9.LMd1z4yEMvcfWZxsWkHdZ_76J1OTUdrhIVml7JiMe4Q'
+
 
 class ExampleTest(HttpUser):
     # @task
     # def home(self):
     #     self.client.request(method="GET", url="/home")
     wait_time = between(1, 3)
-    token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2NjUzOTE5OSwianRpIjoiM2JlYmYwN2EtYzdmYi00NTk2LWJiYzMtMjdlOTk5ZjNmMmVjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjY2NTM5MTk5LCJleHAiOjE2NjY1NDAwOTl9.6RKA7npMqrpCdHRUmE_2ZtyVmCXs436TzZ52t1pWlZI'
 
-    def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        self.login()
+    # def on_start(self):
+    #     """ on_start is called when a Locust start before any task is scheduled """
+    #     # self.login()
 
-    def login(self):
-        print('entro al login')
-        post_data = {'username':'admin', 'password':'admin2022'}
-        response = self.client.post('http://127.0.0.1:5000/api/auth/login', post_data, catch_response=True)
-        print('respondio')
-        data= response.json()
-        print(data)
-        token = data['token']
-        # token = response.content.token
+    # def login(self):
+    #     print('entro al login')
+    #     post_data = {'username':'admin', 'password':'admin2022'}
+    #     response = self.client.post('http://127.0.0.1:5000/api/auth/login', post_data, catch_response=True)
+    #     print('respondio')
+    #     response = conn.getresponse()
+    #     print(data)
+    #     token = data['token']
+    #     # token = response.content.token
 
     @task
     def submit(self):
@@ -35,4 +36,4 @@ class ExampleTest(HttpUser):
         payload={
             "newFormat": "m4a"
         }
-        r = self.client.post("http://127.0.0.1:5000/api/tasks",headers=headers, files=myfile ,data=payload, verify=False)
+        r = self.client.post("http://35.222.194.77:5000/api/tasks",headers=headers, files=myfile ,data=payload, verify=False)
