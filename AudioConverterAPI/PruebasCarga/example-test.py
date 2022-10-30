052f2f596be9a8621532e3ed5b6dccec59174f1b
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2022-10-30 10:41:10 -0500

    Stress test

--- AudioConverterAPI/PruebasCarga/example-test.py
+++ AudioConverterAPI/PruebasCarga/example-test.py
@@ -1,6 +1,6 @@
 from locust import HttpUser, task
 
-token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2NjQ3OTgzNCwianRpIjoiMjZhNjZlNTQtZjM2ZC00NDBjLTkyZDItMDczOGY2NDJhZGZlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjY2NDc5ODM0LCJleHAiOjE2NjY0ODA3MzR9.whkNm5ERcwKSepe8NKuun-RHPfwpgNbcvGWplwMafI4'
+token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2NzE0NDQxMiwianRpIjoiODBjYzI1OTEtMzQ4MC00MDllLTk4NjEtOTE0OGU0ZjIwZjVjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjY3MTQ0NDEyLCJleHAiOjE2NjcxNDUzMTJ9.aQ_QU0DEqzxGZJePqhWRHVSARqtIU2kthuUCk-M1mrQ'
 
 
 class ExampleTest(HttpUser):
@@ -8,7 +8,7 @@ class ExampleTest(HttpUser):
     @task
     def submit(self):
         print('entro al testtt')
-        attach = open('audio.mp3', 'rb')
+        attach = open('voz.mp3', 'rb')
         r = self.client.post("http://127.0.0.1:5000/api/tasks", {
            'Authentication': 'Bearer '+ token,
            'newFormat': 'm4a',
